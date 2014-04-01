a31e31f6b4cce7e9e58d2db3bbedc59136db7245
Nick Price <nick.price@clock.co.uk>
2014-04-01 09:29:05 +0100

    Simplified logic to only use available data

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var Widget = new require('hud-widget')
   , widget = new Widget()
   , getSpreadsheet = require('./lib/get-spreadsheet')
-  , moment = require('moment')
-  , _ = require('lodash')
 
 var utilisationOptions =
     { debug: true
@@ -13,7 +11,6 @@ var utilisationOptions =
     , spreadsheetId: process.env.SPREADSHEET_ID
     , worksheetId: process.env.WORKSHEET_ID
     }
-  , dateFormat = 'MMMM YYYY'
 
 widget.get('/', function (req, res) {
 
@@ -23,28 +20,17 @@ widget.get('/', function (req, res) {
       return res.send(500, { error: err })
     }
 
-    var monthTitle
+    var dataCount = Object.keys(rows['2']).length
+      , monthTitle = rows['1'][dataCount].substr(0, 3)
       , prevMonths =
-        [ moment().subtract('months', 2).format(dateFormat)
-        , moment().subtract('months', 1).format(dateFormat)
+        [ rows['2'][dataCount - 1]
+        , rows['2'][dataCount]
         ]
 
-    if (typeof rows['1'] !== 'undefined' && typeof rows['2'] !== 'undefined') {
-
-      _.each(rows['1'], function (month, key) {
-        if (prevMonths[0] === month) {
-          prevMonths[0] = rows['2'][key] || -1
-        }
-        else if (prevMonths[1] === month) {
-          monthTitle = month.substr(0,3)
-          prevMonths[1] = rows['2'][key] || -1
-        }
-      })
-
+    if (prevMonths.length === 2) {
       res.render('index', { title: 'Utilisation', months: prevMonths, monthTitle: monthTitle })
 
     } else {
-
       console.error('Unsupported spreadsheet layout')
       return res.send(500, { error: 'Unsupported spreadsheet layout' })
 
